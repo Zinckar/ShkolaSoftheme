de47555b1a97ea9176c5cb6fb654f344d3d3fef7
Zinckar <zhakobin@gmail.com>
2017-09-13 01:33:29 +0300

    created solution

---
+++ CSharpHW/Lection1/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Hello, world!");
+            Console.ReadKey();
+        }
+    }
+}
